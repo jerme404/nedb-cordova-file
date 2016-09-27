d7f438179513b4fda0672a11ef810686f39e14d9
Ryan Lindgren <ryan.lindgren@gmail.com>
2016-09-27 16:46:52 -0400

    update storage.js
    updates storage.js to use nedb on top of the cordova-file plugin as if the browser were Node.js

--- browser-version/browser-specific/lib/storage.js
+++ browser-version/browser-specific/lib/storage.js
@@ -1,95 +1,261 @@
 /**
  * Way data is stored for this database
  * For a Node.js/Node Webkit database it's the file system
- * For a browser-side database it's localforage, which uses the best backend available (IndexedDB then WebSQL then localStorage)
+ * For a browser-side database it's the device file system, exposed via the `cordova-file` plugin
  *
  * This version is the browser version
  */
 
-var localforage = require('localforage')
-
-// Configure localforage to display NeDB name for now. Would be a good idea to let user use his own app name
-localforage.config({
-  name: 'NeDB'
-, storeName: 'nedbdata'
-});
-
-
-function exists (filename, callback) {
-  localforage.getItem(filename, function (err, value) {
-    if (value !== null) {   // Even if value is undefined, localforage returns null
-      return callback(true);
+var storage = {
+  exists (_path, cb) {
+    if (_path.indexOf('.') > 0) {
+      _getFile(_path, false, (err, file) => cb(!!file && !err));
     } else {
-      return callback(false);
+      _getDir(this._rootFS, _path, false, (err, dir) => cb(!!dir && !err));
     }
-  });
-}
+  },
+  rename (oldPath, newPath, cb) {
+    var oldDirPath = oldPath.split('/');
+    var newDirPath = newPath.split('/');
+    var oldFilename = oldDirPath.pop();
+    var newFilename = newDirPath.pop();
+    oldDirPath = oldDirPath.join('/');
+    newDirPath = newDirPath.join('/');
 
-
-function rename (filename, newFilename, callback) {
-  localforage.getItem(filename, function (err, value) {
-    if (value === null) {
-      localforage.removeItem(newFilename, function () { return callback(); });
+    if (oldPath.indexOf('.') > 0) {
+      // move/rename file
+      if (!newDirPath || oldDirPath === newDirPath) {
+        // rename file
+        _renameFile(oldPath, newPath, cb);
+      } else {
+        // move file
+        _moveFile(oldPath, newPath, cb);
+      }
     } else {
-      localforage.setItem(newFilename, value, function () {
-        localforage.removeItem(filename, function () { return callback(); });
-      });
+      // move/rename directory
+      if (!newDirPath || oldDirPath === newDirPath) {
+        // rename directory
+        _renameDir(oldPath, newPath, cb);
+      } else {
+        // move directory
+        _moveDir(oldPath, newPath, cb);
+      }
     }
+  },
+  writeFile (file, data, cb, isAppend) {
+    if (typeof file === 'string') {
+      _getFile(file, true, (err, fileObject) => fileObject ? _writeFile(fileObject, data, !!isAppend, cb) : cb(err));
+    } else {
+      _writeFile(file, data, !!isAppend, cb);
+    }
+  },
+  unlink (path, cb) {
+    if (_path.indexOf('.') > 0) {
+      _getFile(_path, false, (err, file) => file ? _removeFile(file, cb) : cb());
+    } else {
+      _getDir(this._rootFS, _path, false, (err, dir) => dir ? _removeDir(dir, cb) : cb());
+    }
+  },
+  appendFile (file, data, cb) {
+    this.writeFile(file, data, cb, true);
+  },
+  readFile (file, cb) {
+    if (typeof file === 'string') {
+      _getFile(file, true, (err, fileObject) => !err ? _readFile(fileObject, cb) : cb(err));
+    } else {
+      _readFile(file, cb);
+    }
+  },
+  mkdirp (_path, cb) {
+    _getDir(this._rootFS, _path, true, cb);
+  },
+  init (rootPath, cb) {
+    window.resolveLocalFileSystemURL(rootPath, (rootFS) => cb(null, (this._rootFS = rootFS)), cb);
+  }
+};
+
+function _readFile (fileObject, cb) {
+  fileObject.file(function (file) {
+    var reader = new FileReader();
+    reader.onloadend = function () {
+      cb(null, this.result);
+    };
+    reader.onerror = cb;
+    reader.readAsText(file);
+  }, cb);
+}
+
+function _writeFile (fileObject, date, isAppend, cb) {
+  fileObject.createWriter(function (fileWriter) {
+    fileWriter.onwriteend = function (res) {
+      cb(null, res);
+    };
+    fileWriter.onerror = cb;
+
+    var blob = new Blob([data], { type: 'text/plain' });
+    if (isAppend) {
+      try {
+        fileWriter.seek(fileWriter.length);
+        fileWriter.write(blob);
+      } catch (e) {
+        cb(e);
+      }
+    } else {
+      fileWriter.write(blob);
+    }
+  }, cb);
+}
+
+function _removeFile (fileObject, cb) {
+  fileObject.remove((() => cb()), cb);
+}
+
+function _removeDir (_path, cb) {
+  _getDir(storage._rootFS, _path, false, (err, dir) => !err ? dir.remove((() => cb()), cb) : cb(err));
+}
+
+function _renameFile (oldPath, newName, cb) {
+  var paths = oldPath.split('/');
+  var filename = paths.pop();
+  var newFilename = newName.split('/').pop();
+  _getDir(storage._rootFS, oldPath, false, (err, dir) => {
+    if (err) return cb(err);
+    dir.getFile(filename, {}, (fileEntry) => {
+      fileEntry.moveTo(dir, newFilename);
+      cb(null, newFilename);
+    }, cb);
   });
 }
 
-
-function writeFile (filename, contents, options, callback) {
-  // Options do not matter in browser setup
-  if (typeof options === 'function') { callback = options; }
-  localforage.setItem(filename, contents, function () { return callback(); });
+function _renameDir (oldPath, newName, cb) {
+  // var paths = oldPath.split('/');
+  // var filename = paths.pop();
+  // var newFilename = newName.split('/').pop();
+  // _getDir(storage._rootFS, oldPath, false, (err, dir) => {
+  //  if (!err) {
+  //    dir.getFile(filename, {}, (fileEntry) => {
+  //      fileEntry.moveTo(dir, newFilename);
+  //      cb(null, newFilename);
+  //    });
+  //  } else {
+  //    cb(err);
+  //  }
+  // });
 }
 
-
-function appendFile (filename, toAppend, options, callback) {
-  // Options do not matter in browser setup
-  if (typeof options === 'function') { callback = options; }
-
-  localforage.getItem(filename, function (err, contents) {
-    contents = contents || '';
-    contents += toAppend;
-    localforage.setItem(filename, contents, function () { return callback(); });
+function _moveFile (oldPath, newName, cb) {
+  var paths = oldPath.split('/');
+  var filename = paths.pop();
+  var newPaths = newName.split('/');
+  var newFilename = newPaths.pop();
+  _getDir(storage._rootFS, newName, true, (err, newDir) => {
+    if (err) return cb(err);
+    _getFile(oldPath, false, (er, fileEntry) => {
+      if (er) return cb(er);
+      fileEntry.moveTo(newDir, newFilename);
+      cb(null, newName);
+    });
   });
 }
 
+function _moveDir (oldPath, newName, cb) {
 
-function readFile (filename, options, callback) {
-  // Options do not matter in browser setup
-  if (typeof options === 'function') { callback = options; }
-  localforage.getItem(filename, function (err, contents) { return callback(null, contents || ''); });
 }
 
-
-function unlink (filename, callback) {
-  localforage.removeItem(filename, function () { return callback(); });
+function _getDir (cwdFS, _path, create, cb) {
+  if (_path.charAt(0) == '.' || _path.charAt(0) == '/') {
+    _path = _path.slice(1);
+  }
+  var paths = _path.split('/');
+  if (paths[paths.length - 1].indexOf('.') > 0) {
+    paths.pop(); // remove the file part of the path so we dont create a dir with filename
+  }
+  cwdFS.getDirectory(paths[0], { create }, (dataDir) => {
+    if (paths.length > 1) {
+      return _getDir(dataDir, paths.slice(1).join('/'), !!create, cb);
+    }
+    return cb(null, dataDir);
+    }, cb);
 }
 
-
-// Nothing to do, no directories will be used on the browser
-function mkdirp (dir, callback) {
-  return callback();
+function _getFile (_path, create, cb) {
+  var paths = _path.split('/');
+  if (paths.length === 1) {
+    storage._rootFS.getFile(_path, { create }, (file) => cb(null, file), cb);
+  } else {
+    _getDir(storage._rootFS, _path, true, (err, dir) => {
+      dir.getFile(paths[paths.length - 1], { create }, ((file) => cb(null, file)), cb);
+    });
+  }
 }
 
+function _ensureInit(cb) {
+  if (!storage._rootFS) {
+    console.warn('Storage not initialized. Call `storage.init(rootFsUrl, callback)`.');
+    if (typeof cordova !== 'undefined' && cordova.file) {
+      console.warn('Using `cordova.file.dataDirectory` until `storage.init` called with new root directory.');
+      storage.init.apply(storage, [cordova.file.dataDirectory, cb]);
+    } else {
+      throw 'Storage not initialized. Call `storage.init(rootFsUrl, callback)`.';
+    }
+  } else {
+    cb(storage._rootFS);
+  }
+}
 
-// Nothing to do, no data corruption possible in the brower
-function ensureDatafileIntegrity (filename, callback) {
-  return callback(null);
+storage.ensureFileDoesntExist = function (file, callback) {
+  storage.exists(file, function (exists) {
+    if (!exists) { return callback(null); }
+
+    storage.unlink(file, function (err) { return callback(err); });
+  });
+};
+
+
+storage.ensureDatafileIntegrity = function (filename, callback) {
+  var tempFilename = filename + '~';
+
+  storage.exists(filename, function (filenameExists) {
+    // Write was successful
+    if (filenameExists) { return callback(null); }
+
+    storage.exists(tempFilename, function (oldFilenameExists) {
+      // New database
+      if (!oldFilenameExists) {
+        return storage.writeFile(filename, '', function (err) { callback(err); });
+      }
+
+      // Write failed, use old version
+      storage.rename(tempFilename, filename, function (err) { return callback(err); });
+    });
+  });
+};
+
+// wrap methods with `_ensureInit` for convenience
+for (var methodName in storage) {
+  if (storage.hasOwnProperty(methodName) && methodName !== 'init') {
+    (function (methodName) {
+      var originalFn = storage[methodName];
+      var wrappedFn = function () {
+        var args = Array.prototype.slice.call(arguments);
+        _ensureInit(() => {
+          originalFn.apply(this, args)
+        });
+      };
+      storage[methodName] = wrappedFn.bind(storage);
+    })(methodName);
+  }
 }
 
 
 // Interface
-module.exports.exists = exists;
-module.exports.rename = rename;
-module.exports.writeFile = writeFile;
-module.exports.crashSafeWriteFile = writeFile;   // No need for a crash safe function in the browser
-module.exports.appendFile = appendFile;
-module.exports.readFile = readFile;
-module.exports.unlink = unlink;
-module.exports.mkdirp = mkdirp;
-module.exports.ensureDatafileIntegrity = ensureDatafileIntegrity;
-
+module.exports.exists = _storage.exists;
+module.exports.rename = _storage.rename;
+module.exports.writeFile = _storage.writeFile;
+module.exports.crashSafeWriteFile = _storage.writeFile;   // No need for a crash safe function in the browser
+module.exports.appendFile = _storage.appendFile;
+module.exports.readFile = _storage.readFile;
+module.exports.unlink = _storage.unlink;
+module.exports.mkdirp = _storage.mkdirp;
+module.exports.ensureFileDoesntExist = _storage.ensureFileDoesntExist;
+module.exports.ensureDatafileIntegrity = _storage.ensureDatafileIntegrity;
